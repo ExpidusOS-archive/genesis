9b1dda863b909fac09a094457f93f183d03e42a5
Tristan Ross <tross@ezoic.com>
2022-10-19 14:45:36 -0700

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -5,31 +5,5 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }:
-    {
-      overlays.default = final: prev: {
-        genesis-shell = (prev.genesis-shell.overrideAttrs (old: {
-          version = self.rev or "dirty";
-          src = builtins.path { name = "genesis-shell"; path = prev.lib.cleanSource ./.; };
-        }));
-      };
-
-      packages = expidus-sdk.lib.forAllSystems (system:
-        let
-          pkgs = expidus-sdk.lib.nixpkgsFor.${system};
-        in {
-          default = (self.overlays.default pkgs pkgs).genesis-shell;
-        });
-
-      devShells = expidus-sdk.lib.forAllSystems (system:
-        let
-          pkgs = expidus-sdk.lib.nixpkgsFor.${system};
-          pkg = self.packages.${system}.default;
-        in
-        {
-          default = pkgs.mkShell {
-            packages = pkg.nativeBuildInputs ++ pkg.buildInputs ++ [ pkgs.expidus-sdk ];
-          };
-        });
-    };
+  outputs = { self, expidus-sdk }: expidus-sdk.lib.mkFlake { inherit self; name = "genesis-shell"; };
 }
