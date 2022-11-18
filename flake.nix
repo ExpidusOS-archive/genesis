ba997cfb16a79b3fe522bc0d4bf8e5d3b389380e
Tristan Ross <tross@ezoic.com>
2022-11-18 13:31:26 -0800

    fix: building

--- flake.nix
+++ flake.nix
@@ -9,13 +9,5 @@
     expidus-sdk.libExpidus.flake.makeOverride {
       inherit self;
       name = "genesis-shell";
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
     };
 }
