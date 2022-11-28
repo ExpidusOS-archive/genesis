dd9d71f0bd9fef4b0fb4c7e798576eb7b13de392
Tristan Ross <tross@ezoic.com>
2022-11-28 14:32:17 -0800

    fix: change supported platforms list

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
   };
 
   outputs = { self, expidus-sdk }:
+    with expidus-sdk.lib;
     expidus-sdk.lib.expidus.flake.makeOverride {
       inherit self;
       name = "genesis-shell";
+      systems = lists.subtractLists (builtins.map (name: "${name}-cygwin") [ "i686" "x86_64" ]) (lists.flatten (builtins.attrValues expidus.system.defaultSupported));
     };
 }
