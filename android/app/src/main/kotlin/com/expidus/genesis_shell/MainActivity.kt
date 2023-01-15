0bd5e19cedd8f7a2601d019150c928057c48bc43
Tristan Ross <tristan.ross@midstall.com>
2023-01-15 09:09:04 -0800

    refactor: flutter

---
+++ android/app/src/main/kotlin/com/expidus/genesis_shell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.expidus.genesis_shell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import expidus_runtimes
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  RuntimesPlugin.register(with: registry.registrar(forPlugin: "RuntimesPlugin"))
+}
